b559c66724f2437e8cd3f1ff9040396bf43a07a8
peng.wang <peng.wang@okg.com>
2023-11-16 17:35:20 +0800

    req

--- index.js
+++ index.js
@@ -19,43 +19,43 @@ getip.get('http://api.proxy.ipidea.io/getBalanceProxyIp?num=10&return_type=json&
     // 23.139.224.203:13692
     // 23.139.224.203:13693
     // 23.139.224.203:13694
-    // const agentList = res.data.data.list;
-    // const testUrl = 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js';
-    // // const testUrl = 'https://static.okx.com/cdnpre/assets/okfe/inner/assets-system-test/0.0.5/d.js';
-    // console.log({ agentList, testUrl });
-    // agentList.forEach(async (item, index) => {
-    //   await sleep(2 * index);
-    //   agentList[index] = 'socks5://' + item;
-    //   const agent = new SocksProxyAgent('socks5://' + item);
-    //   const instance = axios.create({
-    //     httpAgent: agent,
-    //     httpsAgent: agent,
-    //     headers: {
-    //       'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 agent/cdn-precache'
-    //     }
-    //   });
-    //   // 发送请求
-    //   const { data: { data } } = await instance.get('http://143.92.61.72/utils/getRequestIpInfo')
-    //   const startTime = Date.now();
-    //   instance.get(testUrl)
-    //     .then(response => {
-    //       console.log({
-    //         index,
-    //         status: response.status,
-    //         CloudflareHit: response.headers['cf-cache-status'],
-    //         statusText: response.statusText,
-    //         cfRay: response.headers['cf-ray'],
-    //         ipInfo: data,
-    //         time: Date.now() - startTime,
-    //       });
-    //       if (index === 0) {
-    //         console.log(response.data, 'resssssssss');
-    //       }
-    //     })
-    //     .catch(error => {
-    //       console.error('get URL error:', error?.cause);
-    //     });
-    // })
+    const agentList = res.data.data.list;
+    const testUrl = 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js';
+    // const testUrl = 'https://static.okx.com/cdnpre/assets/okfe/inner/assets-system-test/0.0.5/d.js';
+    console.log({ agentList, testUrl });
+    agentList.forEach(async (item, index) => {
+      await sleep(2 * index);
+      agentList[index] = 'socks5://' + item;
+      const agent = new SocksProxyAgent('socks5://' + item);
+      const instance = axios.create({
+        httpAgent: agent,
+        httpsAgent: agent,
+        headers: {
+          'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 agent/cdn-precache'
+        }
+      });
+      // 发送请求
+      const { data: { data } } = await instance.get('http://143.92.61.72/utils/getRequestIpInfo')
+      const startTime = Date.now();
+      instance.get(testUrl)
+        .then(response => {
+          console.log({
+            index,
+            status: response.status,
+            CloudflareHit: response.headers['cf-cache-status'],
+            statusText: response.statusText,
+            cfRay: response.headers['cf-ray'],
+            ipInfo: data,
+            time: Date.now() - startTime,
+          });
+          if (index === 0) {
+            console.log(response.data, 'resssssssss');
+          }
+        })
+        .catch(error => {
+          console.error('get URL error:', error?.cause);
+        });
+    })
   }).catch(error => {
     console.error('wrap error:', error);
   });
