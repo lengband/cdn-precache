223bf3b497b7f0b8cadd24a6629b0f35643e3b54
peng.wang <peng.wang@okg.com>
2023-11-17 17:58:59 +0800

    feat:  firstrun

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const path = require('path');
 const axios = require('axios');
-const request = require('./request');
+const { request } = require('./request');
 const { projectWhiteList, cdnBaseUrl } = require('./strategy');
 
 const axiosInstance = axios.create();
@@ -9,16 +9,9 @@ class Precache {
 
   dataDir = 'data';
 
-  async run() {
-    const { data: resData } = await axiosInstance.get(
-      'https://api.smartproxy.cn/web_v1/ip/get-ip-v3?app_key=5d884abaf2ac978d71f6e2c9987e1508&pt=9&num=10&ep=&cc=SG&state=&city=&life=30&protocol=1&format=json&lb=%5Cr%5Cn'
-    );
-    const agentList = resData.data.list;
-    const testUrl =
-      'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js';
-    agentList.forEach((agentUrl, i) =>
-      request(testUrl, agentUrl, { showContent: i === 0, showIp: true })
-    );
+  projectContentwebMap = {
+    'okfe/p2p': true, // https://static.coinall.ltd/cdn/assets/okfe/p2p/contentweb/9.2.304/asset-manifest.json
+    'okfe/login': true,
   }
 
   async start() {
@@ -53,8 +46,38 @@ class Precache {
   async diffProjectManifest(projectName, version) {
     const url = `${cdnBaseUrl}/cdn/assets/okfe/${projectName}/${version}/asset-manifest.json`;
     const { data: manifestRes } = await axiosInstance.get(url);
-    await fs.writeFileSync(`${this.dataDir}/${projectName}.json`, JSON.stringify(manifestRes, null, 2),'utf-8');
-    console.log({ manifestRes });
+
+    // 与本地文件对比，对比 files: string[] 字段
+    const localManifest = JSON.parse(
+      fs.readFileSync(`${this.dataDir}/${projectName}.json`, 'utf-8')
+    );
+    const diffFiles = manifestRes.files.filter(
+      (file) => !localManifest.files.includes(file)
+    );
+    if (diffFiles.length) {
+      console.log('diffFiles:', diffFiles);
+      diffFiles.forEach((file) => {
+        const assetUrl = this.getAssetUrl({ version, project: projectName, file, enableContentHashBuild: manifestRes.enableContentHashBuild });
+        request.requestEntry(assetUrl, projectName, { showContent: true })
+      });
+    }
+    // await fs.writeFileSync(`${this.dataDir}/${projectName}.json`, JSON.stringify(manifestRes, null, 2),'utf-8'); // DEBUG
+  }
+
+  getAssetUrl({ version, enableContentHashBuild, project, file }) {
+    const isContentweb = this.projectContentwebMap[project];
+    let versionPrefix = `/${version}`;
+    // 兼容 ./index.js 情况
+    let filename = file.startsWith('./') ? file.slice(2) : file;
+    filename = filename.replace('/./', '/'); // 兼容：9.1.16/./index.media_lg_min-1024px.css
+    if (enableContentHashBuild === true || enableContentHashBuild === 'true') {
+      versionPrefix = '';
+    }
+    if (isContentweb) {
+      versionPrefix = '/contentweb';
+    }
+    const assetUrl = `${cdnBaseUrl}/cdn/assets/okfe/${project}${versionPrefix}/${filename}`;
+    return assetUrl
   }
 }
 
--- request.js
+++ request.js
@@ -1,42 +1,96 @@
 const axios = require('axios');
 const proxy = require('socks-proxy-agent');
-
+const { countryWhiteList } = require('./strategy');
 const { SocksProxyAgent } = proxy;
 
-const request = async (targetUrl, agentUrl, { showContent, showIp } = {}) => {
-  const agent = new SocksProxyAgent('socks5://' + agentUrl);
-  const instance = axios.create({
-    httpAgent: agent,
-    httpsAgent: agent,
-    headers: {
-      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 agent/cdn-precache'
-    }
-  });
 
-  const startTime = Date.now();
-  const promiseList = [instance.get(targetUrl)];
-  if (showIp) {
-    promiseList.push(instance.get('http://143.92.61.72/utils/getRequestIpInfo'))
+class Request {
+
+  appKey = '5d884abaf2ac978d71f6e2c9987e1508';
+
+  getProxyUrl(num = 10, cc) {
+    return `https://api.smartproxy.cn/web_v1/ip/get-ip-v3?app_key=${this.appKey}&pt=9&num=${num}&ep=&cc=${cc}&state=&city=&life=30&protocol=1&format=json&lb=%5Cr%5Cn`
   }
-  try {
-    const [response, ipdata] = await Promise.all(promiseList)
-    console.log({
-      status: response.status,
-      CloudflareHit: response.headers['cf-cache-status'],
-      statusText: response.statusText,
-      cfRay: response.headers['cf-ray'],
-      ipInfo: ipdata?.data?.data,
-      time: Date.now() - startTime,
-    });
-    if (showContent) {
-      console.log(response.data, 'resssssssss');
+
+  async proxy(targetUrl, { cc, num })  {
+    const { data: resData } = await axios.get(this.getProxyUrl(num, cc));
+    const agentList = resData.data.list;
+    // targetUrl = 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js';
+    await Promise.all(agentList.map((agentUrl, i) => this.singleFetch(targetUrl, agentUrl, { showContent: i === 0, showIp: true })))
+  }
+
+  async requestEntry(targetUrl, project) {
+    for (const key in countryWhiteList) {
+      await this.proxy(targetUrl, {
+        cc: key,
+        num: countryWhiteList[key].number
+      })
     }
-  } catch (error) {
-    console.error('get URL error:', error?.cause || error?.message);
   }
+
+  async singleFetch(targetUrl, agentUrl, { showContent, showIp } = {}) {
+    const agent = new SocksProxyAgent('socks5://' + agentUrl);
+    const instance = axios.create({
+      httpAgent: agent,
+      httpsAgent: agent,
+      headers: {
+        'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/118.0.0.0 Safari/537.36 agent/cdn-precache'
+      }
+    });
+  
+    const startTime = Date.now();
+    const promiseList = [instance.get(targetUrl)];
+    if (showIp) {
+      promiseList.push(instance.get('http://143.92.61.72/utils/getRequestIpInfo'))
+    }
+    try {
+      const [response, ipdata] = await Promise.all(promiseList)
+      console.log({
+        status: response.status,
+        CloudflareHit: response.headers['cf-cache-status'],
+        statusText: response.statusText,
+        cfRay: response.headers['cf-ray'],
+        ipInfo: ipdata?.data?.data,
+        time: Date.now() - startTime,
+      });
+      if (showContent) {
+        console.log(response.data, 'resssssssss');
+      }
+    } catch (error) {
+      console.error('get URL error:', error?.cause || error?.message);
+    }
+  }
+
+  /**
+   * 并发控制器函数
+   * @param {number} poolLimit 并发限制数
+   * @param {T[]} array 需要进行并发控制的任务数组
+   * @param {(item: T) => Promise<R>} iteratorFn 每个队列任务完成后的回调
+   * @return {Promise<R[]>} 返回所有队列的回调的返回结果
+   */
+    async asyncPool(poolLimit, array, iteratorFn) {
+      const ret = [];
+      const executing = [];
+      for (const item of array) {
+        // 对每个元素执行迭代器函数，返回一个 Promise
+        const p = Promise.resolve().then(() => iteratorFn(item));
+        ret.push(p);
+        // 当数组长度大于等于设定的并发限制时，开始进行并发控制
+        if (poolLimit <= array.length) {
+          // 当 Promise p 完成时，从 executing 数组中移除它
+          const e = p.then(() => executing.splice(executing.indexOf(e), 1));
+          executing.push(e);
+  
+          if (executing.length >= poolLimit) {
+            await Promise.race(executing);
+          }
+        }
+      }
+      return Promise.all(ret);
+    }
 }
 
 
 module.exports = {
-  request
+  request: new Request()
 }
