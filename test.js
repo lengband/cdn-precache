076180226954d85504c699670ae1f105714b1b5c
peng.wang <peng.wang@okg.com>
2023-11-21 16:50:31 +0800

    test

--- test.js
+++ test.js
@@ -2,21 +2,25 @@ const request = require('request-promise');
 
 request
   .get(
-    'http://api.proxy.ipidea.io/getBalanceProxyIp?num=2&return_type=json&lb=1&sb=0&flow=1&regions=sg&protocol=http'
+    'http://api.proxy.ipidea.io/getBalanceProxyIp?num=10&return_type=json&lb=1&sb=0&flow=1&regions=sg&protocol=socks5'
   )
   .then((res) => {
     const resData = JSON.parse(res).data;
     console.log({ resData });
-    request({
-      url: 'https://ipinfo.io',
-      proxy: `http://${resData[0].ip}:${resData[0].port}}`,
+    resData.map((item) => {
+      console.log(`proxy: http://${item.ip}:${item.port}}`);
+
+      request({
+        url: 'https://ipinfo.io',
+        proxy: `socks5://${item.ip}:${item.port}}`,
+      })
+        .then(function(data){
+          console.log('ipinfo:', data);
+          request({
+            url: 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js',
+            proxy: `http://${item.ip}:${item.port}}`,
+          }).then(console.log).catch(console.error)
+        },
+        function(err){ console.error(err)})
     })
-      .then(function(data){
-        console.log('ipinfo:', data);
-        request({
-          url: 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js',
-          proxy: `http://${resData[0].ip}:${resData[0].port}}`,
-        }).then(console.log).catch(console.error)
-      },
-      function(err){ console.error(err)})
   });
