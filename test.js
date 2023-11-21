a3da57a95529bed07c6ee58154fa0bedc758e327
peng.wang <peng.wang@okg.com>
2023-11-21 16:47:37 +0800

    test

--- test.js
+++ test.js
@@ -12,7 +12,11 @@ request
       proxy: `http://${resData[0].ip}:${resData[0].port}}`,
     })
       .then(function(data){
-        console.log(data);
+        console.log('ipinfo:', data);
+        request({
+          url: 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js',
+          proxy: `http://${resData[0].ip}:${resData[0].port}}`,
+        }).then(console.log).catch(console.error)
       },
       function(err){ console.error(err)})
   });
