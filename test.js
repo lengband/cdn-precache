77323dc4f500ae9011ae32fad1aa8bd2a7469e6d
peng.wang <peng.wang@okg.com>
2023-11-21 16:45:15 +0800

    test

--- test.js
+++ test.js
@@ -2,19 +2,14 @@ const request = require('request-promise');
 
 request
   .get(
-    'http://api.proxy.ipidea.io/getBalanceProxyIp?num=10&return_type=json&lb=1&sb=0&flow=1&regions=sg&protocol=http'
+    'http://api.proxy.ipidea.io/getBalanceProxyIp?num=2&return_type=json&lb=1&sb=0&flow=1&regions=sg&protocol=http'
   )
   .then((res) => {
-    console.log(res.data, 'res.data');
-    // res.data 是 类似下面的数据
-    // 23.139.224.203:13692
-    // 23.139.224.203:13693
-    // 23.139.224.203:13694
-    const agentList = res.data.data;
-
+    const resData = JSON.parse(res).data;
+    console.log({ resData });
     request({
       url: 'https://ipinfo.io',
-      proxy: `http://${agentList[0].ip}:${agentList[0].port}]}`,
+      proxy: `http://${resData[0].ip}:${resData[0].port}]}`,
     })
       .then(function(data){
         console.log(data);
