1db3844eb3d40c34c9eaeb7250b93b41a86afa63
peng.wang <peng.wang@okg.com>
2023-11-22 15:41:15 +0800

    test

--- test.js
+++ test.js
@@ -13,13 +13,13 @@ request
       request({
         url: 'https://ipinfo.io',
         proxy: `socks5://${item.ip}:${item.port}}`,
-        resolveWithFullResponse: true
       })
         .then(function(data){
           console.log('ipinfo:', data);
           request({
             url: 'https://www.okx.com/cdn/assets/okfe/inner/assets-system-test/0.0.5/b.js',
             proxy: `http://${item.ip}:${item.port}}`,
+            resolveWithFullResponse: true
           }).then((body) => {
             console.log('body:', body)
           }).catch(console.error)
