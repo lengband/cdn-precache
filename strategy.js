0e442b70dbbd195b425178449fb6331d005a34e3
peng.wang <peng.wang@okg.com>
2023-11-27 20:38:04 +0800

    账密模式

--- strategy.js
+++ strategy.js
@@ -2,36 +2,36 @@ const countryWhiteList = {
   nl: {
     number: 20
   },
-  // ng: {
-  //   number: 10
-  // },
-  // ma: {
-  //   number: 10
-  // },
-  // ar: {
-  //   number: 10
-  // },
-  // br: {
-  //   number: 10
-  // },
-  // mx: {
-  //   number: 20
-  // },
-  // ro: {
-  //   number: 10
-  // },
-  // it: {
-  //   number: 10
-  // },
-  // ru: {
-  //   number: 10
-  // },
-  // fr: {
-  //   number: 10
-  // },
-  // iq: {
-  //   number: 20
-  // },
+  ng: {
+    number: 10
+  },
+  ma: {
+    number: 10
+  },
+  ar: {
+    number: 10
+  },
+  br: {
+    number: 10
+  },
+  mx: {
+    number: 20
+  },
+  ro: {
+    number: 10
+  },
+  it: {
+    number: 10
+  },
+  ru: {
+    number: 10
+  },
+  fr: {
+    number: 10
+  },
+  iq: {
+    number: 20
+  },
 }
 
 const projectWhiteList = [
