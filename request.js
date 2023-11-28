62bee3dc2f12f4f5a0734510cbb53c3068e91ddd
peng.wang <peng.wang@okg.com>
2023-11-28 11:09:46 +0800

    fix

--- request.js
+++ request.js
@@ -4,8 +4,6 @@ const { countryWhiteList } = require('./strategy');
 
 class Request {
 
-  appKey = '5d884abaf2ac978d71f6e2c9987e1508';
-
   fetchState = {
     total: 0,
     success: 0,
@@ -14,7 +12,7 @@ class Request {
   }
 
   getProxyUrl(num = 10, cc) {
-    return `http://api.proxy.ipidea.io/getBalanceProxyIp?num=${num}&return_type=json&lb=1&sb=0&flow=1&regions=${cc}&protocol=socks5`
+    return `http://api.proxy.ipidea.io/getBalanceProxyIp?num=${num}&return_type=json&lb=1&sb=0&flow=1&regions=${cc}&protocol=socks5&spec=1`
   }
 
   async proxy(targetUrl, { cc, num })  {
@@ -33,7 +31,6 @@ class Request {
   async requestEntry(taskList) {
     // taskList = taskList.slice(0, 2);
     this.taskList = taskList;
-    // return this.asyncPool(5, taskList, this.requestByCountry.bind(this));
     return await Promise.all(taskList.map((task, i) => this.requestByCountry(task, i)));
   }
 
@@ -45,7 +42,7 @@ class Request {
         // num: 1
       })
       console.log(`
-      requestByCountry: ${i}cc(${cc}) / ${this.taskList.length} assetUrl(${assetUrl}) done
+      requestByCountry: ${i}cc(${key}) / ${this.taskList.length} assetUrl(${assetUrl}) done
       fetchState(total[${this.fetchState.total}] success[${this.fetchState.success}] error[${this.fetchState.error}] successPercent[${this.fetchState.successPercent})
     `)
     }
@@ -87,35 +84,6 @@ class Request {
       // }
     }
   }
-
-  /**
-   * 并发控制器函数
-   * @param {number} poolLimit 并发限制数
-   * @param {T[]} array 需要进行并发控制的任务数组
-   * @param {(item: T) => Promise<R>} iteratorFn 每个队列任务完成后的回调
-   * @return {Promise<R[]>} 返回所有队列的回调的返回结果
-   */
-    async asyncPool(poolLimit, array, iteratorFn) {
-      const ret = [];
-      const executing = [];
-      for (let i = 0; i < array.length; i++) {
-        const item = array[i];
-        // 对每个元素执行迭代器函数，返回一个 Promise
-        const p = Promise.resolve().then(() => iteratorFn(item, i));
-        ret.push(p);
-        // 当数组长度大于等于设定的并发限制时，开始进行并发控制
-        if (poolLimit <= array.length) {
-          // 当 Promise p 完成时，从 executing 数组中移除它
-          const e = p.then(() => executing.splice(executing.indexOf(e), 1));
-          executing.push(e);
-  
-          if (executing.length >= poolLimit) {
-            await Promise.race(executing);
-          }
-        }
-      }
-      return Promise.all(ret);
-    }
 }
 
 
